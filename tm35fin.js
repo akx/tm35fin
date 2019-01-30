9e008e95a70e17e8ffaf5d647b4cbad6facc6fa4
Aarni Koskela <akx@iki.fi>
2019-01-30 15:31:29 +0200

    Add the rest of the owl

--- tm35fin.js
+++ tm35fin.js
@@ -31,7 +31,8 @@ const size = {
 
 // tile sizes with list index corresponding to tile level (0..9)
 const size_level = [
-  [192000, 96000][(96000, 9600)],
+  [192000, 96000],
+  [96000, 9600],
   [96000, 4800],
   [48000, 4800],
   [48000, 2400],
@@ -120,7 +121,7 @@ const shorten = tile => {
   for (let i = 1; i < 4; i++) {
     const t = shortenedTiles[i - 1];
     LRtiles.push(
-      t.substring(0, t.length - 1) + (/12/.test(t[t.length - 1]) ? "L" : "R")
+      t.substring(0, t.length - 1) + (/[12]/.test(t[t.length - 1]) ? "L" : "R")
     );
   }
 
@@ -130,13 +131,179 @@ const shorten = tile => {
 
   const t = shortenedTiles[7];
   const x =
-    t.substring(0, t.length - 1) + (/ABCD/.test(t[t.length - 1]) ? "L" : "R");
+    t.substring(0, t.length - 1) + (/[ABCD]/.test(t[t.length - 1]) ? "L" : "R");
   shortenedTiles.splice(7, 0, x);
   return shortenedTiles;
+};
+
+const xOffsetMap = {
+  ll: 0,
+  l: 0,
+  ul: 0,
+  sw: 0,
+  w: 0,
+  nw: 0,
+  u: 0.5,
+  c: 0.5,
+  d: 0.5,
+  n: 0.5,
+  "0": 0.5,
+  s: 0.5,
+  lr: 1,
+  r: 1,
+  ur: 1,
+  se: 1,
+  e: 1,
+  ne: 1
+};
+
+const yOffsetMap = {
+  ll: 0,
+  d: 0,
+  lr: 0,
+  sw: 0,
+  s: 0,
+  se: 0,
+  l: 0.5,
+  c: 0.5,
+  r: 0.5,
+  w: 0.5,
+  "0": 0.5,
+  e: 0.5,
+  ul: 1,
+  u: 1,
+  ur: 1,
+  nw: 1,
+  n: 1,
+  ne: 1
+};
+
+/**
+ *
+ * @param {string} tile  tile name, e.g. /L4131F/
+ * @param {*} corner corner of coordinate (ul/u/ur/l/c/r/ll/d/lr or nw/n/ne/w/0/e/sw/s/se)
+ * @returns (x,y) coordinates within tile
+ */
+const tileToXy = (tile, corner = "sw") => {
+  const tileLevel = getTileLevel(tile);
+  const row200_number = rows200.indexOf(tile[0]);
+  const column200_number = parseInt(tile[1]);
+
+  let [x, y] = refpos; // lower left of "K0"
+
+  y += row200_number * size[200][1];
+  x += column200_number * size[200][0];
+
+  if (tile.length > 2) {
+    if (/[34R]/.test(tile[2])) {
+      x += size[100][0];
+    }
+    if (/[24]/.test(tile[2])) {
+      y += size[100][1];
+    }
+  }
+
+  if (tile.length > 3) {
+    if (/[34R]/.test(tile[3])) {
+      x += size[50][0];
+    }
+    if (/[24]/.test(tile[3])) {
+      y += size[50][1];
+    }
+  }
+
+  if (tile.length > 4) {
+    if (/[34R]/.test(tile[4])) {
+      x += size[25][0];
+    }
+    if (/[24]/.test(tile[4])) {
+      y += size[25][1];
+    }
+  }
+
+  if (tile.length > 5) {
+    const offset = 0;
+    if (/[CD]/.test(tile[5])) offset = 1;
+    if (/[EFR]/.test(tile[5])) offset = 2;
+    if (/[GH]/.test(tile[5])) offset = 3;
+    x += size[10][0] * offset;
+    if (/[BDFH]/.test(tile[5])) y += size[10][1];
+  }
+
+  if (tile.length == 7) {
+    if (/[3R]/.test(tile[6])) {
+      x += size[5][0];
+    }
+    if (/[24]/.test(tile[6])) {
+      y += size[5][1];
+    }
+  }
+
+  // now x,y points to lower left of tile
+
+  const x_offset = xOffsetMap[corner];
+  const y_offset = xOffsetMap[corner];
+  if (x_offset === undefined || y_offset === undefined) {
+    throw new Error(`invalid corner ${corner}`);
+  }
+
+  x += size_level[tileLevel][0] * x_offset;
+  y += size_level[tileLevel][1] * y_offset;
+  return [x, y];
+};
+
+/**
+ * @param {string} tile tile name, e.g. /L4131E/
+ * @returns Tile size in meters, e.g. (6000, 6000), throws if broken tile
+ */
+const getTileSize = tile => {
+  const tileLevel = getTileLevel(tile);
+  return size_level[tileLevel];
+};
+
+const legalPerIndex = [
+  /[KLMNPQRSTUVWX]/,
+  /[23456]/,
+  /[1234]/,
+  /[1234]/,
+  /[1234]/,
+  /[ABCDEFGH]/,
+  /[1234]/
+];
+const levels = [-1, -1, 0, 2, 4, 6, 8, 9]; // possible level based on tile length, corrected later for LR tiles
+
+/**
+ * @param {string} tile tile name, e.g. /L4131E/
+ * @returns Tile level index
+ */
+const getTileLevel = tile => {
+  if (tile.length < 2 || tile.length > 7) {
+    throw new Error(`tile ${tile} length must be 2..7`);
+  }
+
+  let LR = false;
+  if (/LR/.test(tile[tile.length - 1])) {
+    if (tile.length === 7) {
+      throw new Error(`tiles of length 7 may not end in L or R`);
+    }
+    LR = true;
+    tile = tile.substr(0, tile.length - 1);
+  }
+
+  for (let i = 0; i < tile.length; i++) {
+    if (!legalPerIndex[i].test(tile[i])) {
+      throw new Error(`illegal character ${tile[i]} (position ${i})`);
+    }
+  }
+
+  return levels[tile.length] + (LR ? 1 : 0);
 };
 
 module.exports = {
   xyToTile,
   xyToTileParts,
+  getTileLevel,
+  getTileSize,
+  tileToXy,
   shorten
 };
--- tm35fin.test.js
+++ tm35fin.test.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai");
-const { xyToTile } = require("./tm35fin");
+const { xyToTile, getTileLevel, tileToXy } = require("./tm35fin");
 
 describe("tm35fin", () => {
   it("converts Turku's coordinates correctly", () => {
@@ -7,5 +7,12 @@ describe("tm35fin", () => {
   });
   it("converts Turku's coordinates correctly, level 6", () => {
     expect(xyToTile(239645, 6712052, 6)).to.equal("L3324");
+  });
+  it("converts tile levels", () => {
+    expect(getTileLevel("L3324")).to.equal(6);
+    expect(getTileLevel("L3324B4")).to.equal(9);
+  });
+  it("converts Turku's tile back correctly", () => {
+    expect(tileToXy("L3324B4")).to.deep.equal([236000, 6711000]);
   });
 });
